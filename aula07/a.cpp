257c874d08db7b047097111bc6177c2ec768de01
Ryan S <ryansouza@usp.br>
2023-05-18 21:12:36 -0300

    Da pra chegar?

---
+++ aula07/a.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define pi pair<int,int>
+#define hashMap unordered_map
+#define minHeap priority_queue<int, vector<int>, greater<int>>
+#define maxHeap priority_queue<int>
+#define print(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+#define printArray(arr) for(auto x : arr) cout << x << ' '; cout << endl;
+#define printStack(s) while(!s.empty()){cout << s.top() << ' '; s.pop();} cout << endl;
+#define printQueue(q) while(!q.empty()){cout << q.front() << ' '; q.pop();} cout << endl;
+#define printMap(m) for(auto x : m) cout << x.first << ' ' << x.second << endl;
+#define _ << ' ' <<
+#define endl '\n'
+
+using namespace std;
+
+vector<int> adj[100005];
+bool visited[100005];
+
+void dfs(int u){
+    visited[u] = true;
+    for(int v : adj[u]){
+        if(not visited[v]){
+            dfs(v);
+        }
+    }
+}
+
+void solve() {
+    int n,m,a,b;
+    cin>>n>>m>>a>>b;
+    while(m--){
+        int u,v;
+        cin>>u>>v;
+        adj[u].pb(v);
+    }
+    dfs(a);
+
+    if (visited[b]) cout << "SIM" << endl;
+    else cout << "NAO" << endl;
+    
+}
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t = 1; //cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
