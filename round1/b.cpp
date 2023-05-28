5018e71ddfaa63b9e7f52831a938095ee8b62dcd
Ryan S <ryansouza@usp.br>
2023-05-28 14:25:05 -0300

    Cifra de César

---
+++ round1/b.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define pi pair<int,int>
+#define hashMap unordered_map
+#define minHeap priority_queue<int, vector<int>, greater<int>>
+#define maxHeap priority_queue<int>
+#define print(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< ' '; cout << endl
+#define printArray(arr) for(auto x : arr) cout << x << ' '; cout << endl;
+#define printStack(s) while(!s.empty()){cout << s.top() << ' '; s.pop();} cout << endl;
+#define printQueue(q) while(!q.empty()){cout << q.front() << ' '; q.pop();} cout << endl;
+#define printMap(m) for(auto x : m) cout << x.first << ' ' << x.second << endl;
+#define _ << ' ' <<
+#define endl '\n'
+
+using namespace std;
+
+const int MAXN = 1e5+5;
+
+void solve() {
+  int n,k;
+  cin>>n>>k;
+  string s;
+  cin >> s;
+  string ans;
+  for(int i = 0; i < n; i++){
+    int ch = s[i] - 'a';
+    char newCh = ((ch + k) % 26);
+    ans += newCh + 'a';
+  }
+
+  cout << ans << endl;
+}
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t = 1; //cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
